e8b27cd33908a1d976c1eec3f81a434fe1a762ec
galuhsatria <galuhsatria219@gmail.com>
2024-01-07 22:31:41 +0800

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div className="max-w-4xl mx-auto px-4 mt-10 text-center min-h-screen">
+      <p className="text-center">Powered by </p>
+      <ul className='flex flex-col gap-3 mt-4'>
+        <li>
+          <p>Quran Quiz Generator Library:</p>
+          <Link href="https://quran.zakiego.com/" className='text-blue-500 underline'>https://quran.zakiego.com/</Link>{' '}
+        </li>
+        <li>
+          <p>Al Quran API:</p>
+          <Link href="https://equran.id/" className='text-blue-500 underline'>https://equran.id/</Link>
+        </li>{' '}
+      </ul>
+    </div>
+  );
+}
